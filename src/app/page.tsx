f49cdbc66fab50a270302dd2a74356cb4ca03c0b
Ernesto Joel Hdz <ernesto@nimbo.mx>
2024-09-30 09:41:37 -0600

    Zustand setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import { Button } from "@/components/Button";
+import useFormStore from "@/stores/form.store";
 
 
 export default function Home() {
+  const count = useFormStore( state => state.count )
+  const increment = useFormStore( state => state.increment )
   return (
     <div>
-      <Button>Button</Button>
+      <div>{count}</div>
+      <Button onClick={increment}>Button</Button>
     </div>
   );
 }
