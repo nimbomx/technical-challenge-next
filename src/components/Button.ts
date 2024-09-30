7d3c1a9d484ced2579e9cf5e916e1a1f3cc8a822
Ernesto Joel Hdz <ernesto@nimbo.mx>
2024-09-30 09:34:14 -0600

    Installing and configuring styled-components

---
+++ src/components/Button.ts
@@ -0,0 +1,14 @@
+'use client'
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background-color: #4A3AFF;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    cursor: pointer;
+`
