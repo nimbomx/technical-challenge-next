242dabb8ea763557597f9caef2dd30cd4b4635b1
Ernesto Joel Hdz <ernesto@nimbo.mx>
2024-09-30 09:50:42 -0600

    update store to use sessionStorage

--- src/stores/form.store.ts
+++ src/stores/form.store.ts
@@ -1,13 +1,32 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface FormStoreState {
   count: number;
   increment: () => void;
 }
 
-const useFormStore = create<FormStoreState>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-}));
+const useFormStore = create<FormStoreState>()(
+    persist(
+        (set) => ({
+            count: 0,
+            increment: () => set((state) => ({ count: state.count + 1 })),
+        }),
+        {
+            name: 'form-storage',
+            storage: createJSONStorage(() => {
+              if (typeof window !== 'undefined') {
+                return sessionStorage;
+              }
+              // Retorna un storage de no operación (no-op) durante la renderización del lado del servidor
+              return {
+                getItem: () => null,
+                setItem: () => {},
+                removeItem: () => {},
+              };
+            }),
+          }
+    )
+  );
 
 export default useFormStore;
